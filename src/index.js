84caf4ac83994ed4306df84385a25bedcff288ce
Lukas <Lukas@DESKTOP-3VP53QK>
2024-07-17 00:36:10 +0200

    style: Update background color in FoodInfoApp.css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import FoodInfoApp from './FoodInfoApp';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <FoodInfoApp />
   </React.StrictMode>,
